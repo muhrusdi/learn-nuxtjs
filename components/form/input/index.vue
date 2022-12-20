bcce5df8b3dbacd0257c0b9d22953ee8baafd564
muhrusdi <mhmrofficial@gmail.com>
2022-12-20 14:26:48 +0700

    feat: custom input component

--- components/form/input/index.vue
+++ components/form/input/index.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
+    <label :for="label" class="text-sm mb-2 inline-block">{{label}}</label>
     <v-text-field
       :label="label"
       outlined
+      :id="label"
       v-model="fields[name]"
       :error-messages="errors"
       @input="vFields[name].$touch()"
       @blur="vFields[name].$touch()"
       :type="inputType"
+      solo
+      depressed
       v-bind="$attrs"
       :append-icon="type === 'password' ? (visiblePass ? 'mdi-eye-off' : 'mdi-eye') : ''"
       @click:append="type === 'password' ? handleSwitch() : null"
@@ -42,3 +46,9 @@ export default {
   }
 }
 </script>
+
+<style>
+.theme--dark.v-text-field--solo > .v-input__control > .v-input__slot {
+  background-color: #282a36;
+}
+</style>
--- components/form/select/index.vue
+++ components/form/select/index.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
+    <label :for="label" class="text-sm mb-2 inline-block">{{label}}</label>
     <v-combobox
       :items="items"
+      :id="label"
       :label="label"
+      solo
       v-model="fields[name]"
       :error-messages="errors"
       @input="vFields[name].$touch()"
@@ -26,3 +29,14 @@ export default {
   },
 }
 </script>
+
+<style>
+.v-text-field--solo > .v-input__control > [role=combobox].v-input__slot {
+  box-shadow: none !important;
+  border-bottom: 1px solid #5b5d66;
+}
+
+.v-text-field--solo.error--text > .v-input__control > [role=combobox].v-input__slot {
+  border-bottom: 1px solid #ff5252 !important;
+}
+</style>
