ed4bfc5e0a6ea5d5f9156e57070dcfed7a9bfdc3
muhrusdi <mhmrofficial@gmail.com>
2022-12-28 13:42:07 +0700

    chore: update auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,11 @@
 
 export default function ({app, redirect, route}) {
   const cookieRes = app.$cookies.get('accessToken')
-  if (!cookieRes) {
-    return redirect('/auth/login')
-  } else {
+  if (cookieRes) {
     if (route.path === "/auth/login" || route.path === "/auth/register" || route.path === "/auth/verification") {
       return redirect('/')
     }
+  } else {
+    return redirect('/auth/login')
   }
 }
