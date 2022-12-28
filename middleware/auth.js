d5aa51cf18a513b283e5b11a1702d5f6921fe96d
muhrusdi <mhmrofficial@gmail.com>
2022-12-28 14:31:39 +0700

    bugfix: fix auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,22 @@
 
 export default function ({app, redirect, route}) {
   const cookieRes = app.$cookies.get('accessToken')
+  const isAutPage = route.path === "/auth/login" || route.path === "/auth/register" || route.path === "/auth/verification"
+
   if (cookieRes) {
-    if (route.path === "/auth/login" || route.path === "/auth/register" || route.path === "/auth/verification") {
+    if (isAutPage) {
       return redirect('/')
     }
   } else {
-    return redirect('/auth/login')
+    switch(route.path) {
+      case "/auth/verification": {
+        return redirect(router.path)
+      }
+      case "/auth/register": {
+        return redirect(router.path)
+      }
+      default:
+      return redirect("/auth/login")
+    }
   }
 }
